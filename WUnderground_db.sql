96531583197b851427c11920b1781fcebf04ddc7
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-09-07 18:28:47 +0530

    Updated Extracting WeatherStations

--- WUnderground_db.sql
+++ WUnderground_db.sql
@@ -9,4 +9,6 @@ VALUES
   (23, 'Isurupaya', 6.89, 79.92),
   (24, 'Borella', 6.93, 79.86),
   (25, 'Kompannaveediya', 6.92, 79.85),
-  (26, 'Angurukaramulla', 7.21, 79.86);
+  (26, 'Angurukaramulla', 7.21, 79.86),
+  (27, 'Hirimbure', 6.054, 80.221),
+  (28, 'Yatiwawala', 7.329, 80.613);
